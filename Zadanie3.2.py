6a2b8efcd19f4cf84f893bdf1754ba076c2bd177
OlaIg <a.iglicka@protonmail.com>
2025-01-18 19:18:23 +0100

    Add loop iterating through dict

--- Zadanie3.2.py
+++ Zadanie3.2.py
@@ -2,3 +2,5 @@ shopping_list = {
     "bakery" : ["bread", "buns", "donut"],
     "fruit&veg" : ["carrot", "celery", "arugula"]
 }
+for shop, products in shopping_list.items():
+    print(f"I'm heading to {shop}, I'm gonna get: {products}.")
